500ceee827328b811b11561cc7e5904f75492fbc
Sybil Amalia Herrera Foessel <syhe3484@rackham3.uppmax.uu.se>
2022-04-01 11:56:26 +0200

    sbatch script for PacBio reads assembly with canu

--- code/01_genome_assembly/01_genome_assembly.sh
+++ code/01_genome_assembly/01_genome_assembly.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -l
-
 #SBATCH -A uppmax2022-2-5
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH --reservation=uppmax2022-2-5_2
 #SBATCH -t 05:00:00
 #SBATCH -J 01Genome_assembly_OUT
 #SBATCH --mail-type=ALL
@@ -14,7 +12,11 @@
 module load bioinfo-tools
 module load canu 
 
-# Commands
- canu -d /home/syhe3484/Sybil/GenomeAnalysis/analyses/02_genome_assembly/ -p assembly genomeSize=3m -pacbio-raw /home/syhe3484/Sybil/GenomeAnalysis/data/raw_data/genomics_data/PacBioreads/*.fastq.gz
-
+#Commands
+canu \
+ -d /home/syhe3484/Sybil/GenomeAnalysis/analyses/02_genome_assembly/ \
+ -p ass_canu01 \
+ genomeSize=3m \
+ -pacbio-raw /home/syhe3484/Sybil/GenomeAnalysis/data/raw_data/genomics_data/PacBio/*.fastq.gz
+ 
               
